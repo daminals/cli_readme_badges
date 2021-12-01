58a7d63c906c25bfe5c51569233925a341c8ebb2
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-12-01 15:43:45 -0500

    readme cli get request

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://shields.io/endpoint")
+    
+    let resp = reqwest::get("https://shields.io/endpoint?")
         .await?
         .json::<HashMap<String, String>>()
         .await?;
@@ -10,3 +11,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn args () -> &'static str {
+    let SchemaVersion = "1";
+    let label = "temp";
+    let color = "temp";
+
+    return format!("SchemaVersion={}&label={}&color={}", SchemaVersion, label, color);
+}
