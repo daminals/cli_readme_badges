42c8dc066311b32c7c8788036cd572888bfaa855
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-12-01 16:40:05 -0500

    github badge generator

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,26 @@ use std::io;
 async fn main(){
     let args_ = args();
     let full_url: &str = &format!("https://img.shields.io/badge/{}",args_)[..];
-    //println!("{}",full_url);
+    let md_form = markdown_format(full_url);
+    println!("{}",md_form);
 }
 
 fn args () -> String {
+    linebreak();
     println!("\nLabel name: ");
     let mut labelAsk = String::new();
     io::stdin()
     .read_line(&mut labelAsk)
     .expect("Failed to read line");
 
+    linebreak();
     println!("\nMessage: ");
     let mut messageAsk = String::new();
     io::stdin()
     .read_line(&mut messageAsk)
     .expect("Failed to read line");
 
+    linebreak();
     println!("\nColor: ");
     let mut colorAsk = String::new();
     io::stdin()
@@ -31,4 +35,20 @@ fn args () -> String {
     let color: &str = colorAsk.trim();
 
     return format!("{}-{}-181717?color={}",label,message,color);
+}
+
+fn markdown_format(link: &str) -> String {
+    linebreak();
+    println!("\nAlt Text: ");
+    let mut alt_text = String::new();
+    io::stdin()
+    .read_line(&mut alt_text)
+    .expect("Failed to read line");
+    let alt_text: &str = alt_text.trim();
+    
+    return format!("![{}]({})", alt_text, link);
+}
+
+fn linebreak () {
+    println!("{}", "=".repeat(50));
 }
